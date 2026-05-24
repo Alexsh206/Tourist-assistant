63790bed7682146f1d09f8220756eb25946f4618
Олексій <145874562+Alexsh206@users.noreply.github.com>
2026-05-24 04:09:23 +0300

    /////

--- travel-assistant-frontend/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar({ onLogout }) {
-    return (
-        <div className="p-4 bg-gray-200">
-            <button onClick={onLogout}>Logout</button>
-        </div>
-    );
-}
